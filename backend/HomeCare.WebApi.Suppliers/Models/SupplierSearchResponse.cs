68f1ac52db75840740f89ed4c4aadb94d59696a8
Paulo Duarte <phduarte87@outlook.com>
2022-05-03 21:14:17 -0300

    inclusão de campos de imagem e descrição

--- backend/HomeCare.WebApi.Suppliers/Models/SupplierSearchResponse.cs
+++ backend/HomeCare.WebApi.Suppliers/Models/SupplierSearchResponse.cs
@@ -11,6 +11,8 @@ public class SupplierSearchResponse
     public long Latitude { get; set; }
     public long Longitude { get; set; }
     public long Range { get; set; }
+    public string Image { get; set; } = "https://thumbs.dreamstime.com/z/home-care-logo-design-template-vector-icon-128771044.jpg";
+    public string Description { get; set; } = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.";
 
     public static SupplierSearchResponse Parse(Supplier supplier)
     {
